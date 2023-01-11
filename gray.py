ee4218fea2bc5e8f6fbb06d0fd9917fd9ca3fc10
Oliver JT <114947555+oliver-jt@users.noreply.github.com>
2023-01-11 19:19:03 +0100

    Update gray.py

--- gray.py
+++ gray.py
@@ -1,9 +1,9 @@
-def run(code):
+def run(code, main = True):
     try:
         import time
-        counter = 0
-        memory = [0]
-        if_statements = ["if_equal", "if_lower", "if_higher", "if_not_equal", "if_not_lower", "if_not_higher"]
+        line = 0
+        varNames = []
+        varValues = []
         def join(strings):
             try:
                 splitted_strings = []
@@ -56,22 +56,20 @@ def run(code):
                 splitted_calculation.append(chars)
                 for j,i in enumerate(splitted_calculation):
                     if not i in arithmeticoperations:
-                        splitted_calculation[j] = get_value(i)
+                        splitted_calculation[j] = get_value(i.replace(" ","").replace("\t",""))
                 calculation = ""
                 for i in splitted_calculation:
                     calculation += str(i)
                 return eval(calculation)
-            except:
-                print("error: can't calculate " + str(calculation))
+            except Exception as e:
+                print("error: can't calculate " + str(calculation) + ":" + str(e))
         def get_value(raw_value):
             try:
                 if str(raw_value)[0] == '"' and str(raw_value)[-1] == '"':
                     return raw_value[1:len(raw_value)-1]
                 elif str(raw_value)[0] == ':':
-                    if int(get_value(raw_value[1:])) + 1 > len(memory):
-                        while int(get_value(raw_value[1:])) + 1 > len(memory):
-                            memory.append(0)
-                    return get_value(memory[int(get_value(raw_value[1:]))])
+                    if get_value(raw_value[1:]) in varNames:
+                        return varValues[varNames.index(get_value(raw_value[1:]))]
                 elif str(raw_value)[0] == '(' and str(raw_value)[-1] == ')':
                     return calculate(raw_value[1:len(raw_value)-1])
                 elif str(raw_value)[0] == '[' and str(raw_value)[-1] == ']':
@@ -122,67 +120,111 @@ def run(code):
                     chars += i
             splitted_line.append(chars)
             return splitted_line
-        while(counter < len(code)):
+        while(line < len(code)):
             try:
-                i = code[counter]
+                i = code[line]
                 if not i.replace(" ","").replace("\t","") == "":
                     if not i[0:2] == "//":
                         splitted_code_line = split_line(i)
                         if splitted_code_line[0] == "print":
                             print(get_value(splitted_code_line[1]))
                         elif splitted_code_line[0] == "set":
-                            if int(get_value(splitted_code_line[1])) + 1 > len(memory):
-                                while int(get_value(splitted_code_line[1])) + 1 > len(memory):
-                                    memory.append(0)
-                            memory[int(get_value(splitted_code_line[1]))] = get_value(splitted_code_line[2])
+                            if get_value(splitted_code_line[1]) in varNames:
+                                varValues[varNames.index(get_value(splitted_code_line[1]))] = get_value(splitted_code_line[2])
+                            else:
+                                varNames.append(get_value(splitted_code_line[1]))
+                                varValues.append(get_value(splitted_code_line[2]))
                         elif splitted_code_line[0] == "goto":
                             if not str(splitted_code_line[1])[0] == "$":
-                                counter = int(get_value(splitted_code_line[1])) - 2
+                                line = int(get_value(splitted_code_line[1])) - 2
                             else:
-                                counter = int(code.index("$" + get_value(splitted_code_line[1][1:])))
+                                line = int(code.index("$" + get_value(splitted_code_line[1][1:])))
                         elif splitted_code_line[0] == "wait":
                             time.sleep(int(get_value(splitted_code_line[1])))
-                        elif splitted_code_line[0] == "if_equal":
-                            if get_value(splitted_code_line[1]) != get_value(splitted_code_line[2]):
-                                counter += 1
-                                while split_line(code[counter])[0] in if_statements:
-                                    counter += 1
-                        elif splitted_code_line[0] == "if_lower":
-                            if float(get_value(splitted_code_line[1])) >= float(get_value(splitted_code_line[2])):
-                                counter += 1
-                                while split_line(code[counter])[0] in if_statements:
-                                    counter += 1
-                        elif splitted_code_line[0] == "if_higher":
-                            if float(get_value(splitted_code_line[1])) <= float(get_value(splitted_code_line[2])):
-                                counter += 1
-                                while split_line(code[counter])[0] in if_statements:
-                                    counter += 1
-                        elif splitted_code_line[0] == "if_not_equal":
-                            if get_value(splitted_code_line[1]) == get_value(splitted_code_line[2]):
-                                counter += 1
-                                while split_line(code[counter])[0] in if_statements:
-                                    counter += 1
-                        elif splitted_code_line[0] == "if_not_lower":
-                            if float(get_value(splitted_code_line[1])) < float(get_value(splitted_code_line[2])):
-                                counter += 1
-                                while split_line(code[counter])[0] in if_statements:
-                                    counter += 1
-                        elif splitted_code_line[0] == "if_not_higher":
-                            if float(get_value(splitted_code_line[1])) > float(get_value(splitted_code_line[2])):
-                                counter += 1
-                                while split_line(code[counter])[0] in if_statements:
-                                    counter += 1
+                        elif splitted_code_line[0] == "if":
+                            def equal_as_float():
+                                boolean = False
+                                try:
+                                    boolean = float(get_value(splitted_code_line[1])) != float(get_value(splitted_code_line[2]))
+                                except:
+                                    boolean = False
+                                return boolean
+                            if splitted_code_line[2] == "==":
+                                if get_value(splitted_code_line[1]) != get_value(splitted_code_line[3]):
+                                    expected_ends = 1
+                                    while expected_ends > 0:
+                                        line += 1
+                                        if split_line(code[line])[0] == "if":
+                                             expected_ends += 1
+                                        if split_line(code[line])[0] == "end_if" or split_line(code[line])[0] == "else":
+                                            expected_ends -= 1
+                            if splitted_code_line[2] == "!=":
+                                if get_value(splitted_code_line[1]) == get_value(splitted_code_line[3]):
+                                    expected_ends = 1
+                                    while expected_ends > 0:
+                                        line += 1
+                                        if split_line(code[line])[0] == "if":
+                                             expected_ends += 1
+                                        if split_line(code[line])[0] == "end_if" or split_line(code[line])[0] == "else":
+                                            expected_ends -= 1
+                            if splitted_code_line[2] == ">":
+                                if not get_value(splitted_code_line[1]) > get_value(splitted_code_line[3]):
+                                    expected_ends = 1
+                                    while expected_ends > 0:
+                                        line += 1
+                                        if split_line(code[line])[0] == "if":
+                                             expected_ends += 1
+                                        if split_line(code[line])[0] == "end_if" or split_line(code[line])[0] == "else":
+                                            expected_ends -= 1
+                            if splitted_code_line[2] == "<":
+                                if not get_value(splitted_code_line[1]) < get_value(splitted_code_line[3]):
+                                    expected_ends = 1
+                                    while expected_ends > 0:
+                                        line += 1
+                                        if split_line(code[line])[0] == "if":
+                                             expected_ends += 1
+                                        if split_line(code[line])[0] == "end_if" or split_line(code[line])[0] == "else":
+                                            expected_ends -= 1
+                            if splitted_code_line[2] == "!>":
+                                if get_value(splitted_code_line[1]) > get_value(splitted_code_line[3]):
+                                    expected_ends = 1
+                                    while expected_ends > 0:
+                                        line += 1
+                                        if split_line(code[line])[0] == "if":
+                                             expected_ends += 1
+                                        if split_line(code[line])[0] == "end_if" or split_line(code[line])[0] == "else":
+                                            expected_ends -= 1
+                            if splitted_code_line[2] == "!<":
+                                if get_value(splitted_code_line[1]) < get_value(splitted_code_line[3]):
+                                    expected_ends = 1
+                                    while expected_ends > 0:
+                                        line += 1
+                                        if split_line(code[line])[0] == "if":
+                                             expected_ends += 1
+                                        if split_line(code[line])[0] == "end_if" or split_line(code[line])[0] == "else":
+                                            expected_ends -= 1
                         elif splitted_code_line[0] == "input":
-                            if int(get_value(splitted_code_line[1])) + 1 > len(memory):
-                                while int(get_value(splitted_code_line[1])) + 1 > len(memory):
-                                    memory.append(0)
-                            memory[int(get_value(splitted_code_line[1]))] = input(get_value(splitted_code_line[2]))
+                            if get_value(splitted_code_line[2]) in varNames:
+                                varValues[varNames.index(get_value(splitted_code_line[2]))] = input(get_value(splitted_code_line[1]))
+                            else:
+                                varNames.append(get_value(splitted_code_line[2]))
+                                varValues.append(input(get_value(splitted_code_line[1])))
+                        elif splitted_code_line[0] == "else":
+                            cline = line + 1
+                            expected_ends = 1
+                            while expected_ends > 0:
+                                line += 1
+                                if split_line(code[line])[0] == "if":
+                                    expected_ends += 1
+                                if split_line(code[line])[0] == "end_if" or split_line(code[line])[0] == "else":
+                                    expected_ends -= 1
+                            line += 1
                         else:
-                            if not splitted_code_line[0][0] == "$":
-                                print("error in line " + str(counter + 1) + " unknown syntax: " + splitted_code_line[0])
+                            if not splitted_code_line[0][0] == "$" and not splitted_code_line[0] in ["end_if"]:
+                                print("error in line " + str(line + 1) + " unknown syntax: " + splitted_code_line[0])
             except Exception as e:
-                print("runtime error in line " + str(counter + 1) + ": " + str(e))
-            counter += 1
+                print("runtime error in line " + str(line + 1) + ": " + str(e))
+            line += 1
     except Exception as e:
         print("gray has crashed: " + str(e))
 try:
